caac09f37a32d4b5c8c50a1eb8717668dcc5e110
Diana <diana.iminza@ronforddigital.com>
2023-06-05 11:44:38 +0300

    slider

---
+++ Reelfruit/data.js
@@ -0,0 +1,22 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: '/wp-content/uploads/2021/03/Home-Page-Sliders-04.png',
+     title:"Home Page Sliders-04",
+      bg: "transparent",
+    },
+    {
+      id: 2,
+      img:'wp-content/uploads/2021/12/WhatsApp-Image-2021-09-22-at-12.43.59-PM.jpg',
+      title: "WhatsApp Image 2021-09-22 at 12.43.59 PM",
+    
+      bg: "transparent",
+    },
+    
+    {
+      id: 3,
+      img: 'wp-content/uploads/2021/04/Home-Page-Sliders-Edited-02-1-scaled.jpg',
+      title:"Home Page Sliders Edited-02 (1)",
+      bg: "transparent",
+    },
+  ];
