ad3908e6a7e7d6692f17e94b49c8e6dd2d643cdf
Diana <diana.iminza@ronforddigital.com>
2023-07-14 16:55:32 +0300

    OGV

---
+++ OGV/xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://Reelfruit.com</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://Reelfruit.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://Reelfruit.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://Reelfruit.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://Reelfruit.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://Reelfruit.com/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
